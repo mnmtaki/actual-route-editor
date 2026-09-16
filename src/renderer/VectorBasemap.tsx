ec354e5fc800c516742a8a596b4a9bbddbfea1cb
mnmtaki <323608206+mnmtaki@users.noreply.github.com>
2026-09-16 21:18:20 +0800

    Render sunken AARC TextTags below transit lines

--- src/renderer/VectorBasemap.tsx
+++ src/renderer/VectorBasemap.tsx
@@ -2,6 +2,7 @@ import type { ActualRouteProject, Road } from '../data/model'
 import { sortedVectorBasemapObjects } from '../data/roads'
 import { AarcTerrainTransitionsArtwork, BasemapPathArtwork } from './BasemapPaths'
 import { RoadArtwork } from './Roads'
+import { AarcTextTagsLayer } from './AarcTextTags'
 
 export function VectorBasemapLayer({
   project,
@@ -37,6 +38,7 @@ export function VectorBasemapLayer({
       return <RoadArtwork key={`road-${road.id}`} road={road} project={project} presentation={presentation} selected={selectedId === road.id} hitRadius={hitRadius} onPointerDown={onRoadPointerDown} onPointPointerDown={onRoadPointPointerDown} />
     })}
     <AarcTerrainTransitionsArtwork project={project} part="body" />
+    <AarcTextTagsLayer project={project} presentation={presentation} mode="sunken" />
     {draft && <RoadArtwork road={draft} project={project} presentation={false} selected hitRadius={hitRadius} onPointerDown={onRoadPointerDown} onPointPointerDown={onRoadPointPointerDown} />}
   </g>
 }
